f1df4836aa141051fc84dc5f6df3c41d6f11522b
James McCann <jamesredux@gmail.com>
2019-09-17 20:52:42 +0700

    add partials and tidy routes

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -3,20 +3,26 @@ require 'test_helper'
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
 
   test "should get root" do 
-    get root_url
+    get root_path
     assert_response :success
   end
     
   test "should get home" do
-    get static_pages_home_url
+    get root_path
     assert_response :success
     assert_select "title", "Odin-Facebook"
   end
 
   test "should get about" do
-    get static_pages_about_url
+    get about_path
     assert_response :success
     assert_select "title", "About | Odin-Facebook"
   end
+
+  test "should get contact" do 
+    get contact_path
+    assert_response :success
+    assert_select "title", "Contact | Odin-Facebook"
+  end  
 
 end
