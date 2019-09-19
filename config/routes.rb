6fe70a4590403484d532c0a3d492d2d4fd12d42b
James McCann <jamesredux@gmail.com>
2019-09-19 19:50:58 +0700

    add new user route and page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  get 'users/new'
   root 'static_pages#home'
 
+  get '/signup', to: 'users#new'
   get '/about', to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
