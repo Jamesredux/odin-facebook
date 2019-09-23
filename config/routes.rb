78e26496daef314a4c20064f6febc7937c8d1661
James McCann <jamesredux@gmail.com>
2019-09-23 20:00:10 +0700

    add users routes and show view

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,6 @@ Rails.application.routes.draw do
   get '/signup', to: 'users#new'
   get '/about', to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :users
   
 end
