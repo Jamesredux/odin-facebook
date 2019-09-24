483c1016480a53f335df85976e8f1f7adeec777f
James McCann <jamesredux@gmail.com>
2019-09-24 19:34:27 +0700

    add user show page

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get 'users/new'
   root 'static_pages#home'
 
-  get '/signup', to: 'users#new'
+  
   get '/about', to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
   resources :users
