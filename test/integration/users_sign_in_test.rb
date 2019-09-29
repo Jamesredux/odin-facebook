43836c580aa3eab4f2f5a052285acf85ba7e1f66
James McCann <jamesredux@gmail.com>
2019-09-29 20:22:27 +0700

    style remember me checkbox

--- test/integration/users_sign_in_test.rb
+++ test/integration/users_sign_in_test.rb
@@ -1,6 +1,7 @@
 require 'test_helper'
 
 class UsersSignInTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
 
   test "sign_in with invalid informatio" do 
   	get new_user_session_path
@@ -29,7 +30,10 @@ class UsersSignInTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", user_path(@user)
     delete destroy_user_session_path
     assert_redirected_to root_url
+    #Simulate user logging out twice  in 2 windows
+    delete destroy_user_session_path
     follow_redirect!
+
     assert_select "a[href=?]", new_user_session_path
     assert_select "a[href=?]", destroy_user_session_path,      count: 0
     assert_select "a[href=?]", user_path(@user), count: 0                              
@@ -46,6 +50,6 @@ class UsersSignInTest < ActionDispatch::IntegrationTest
     assert_not flash.empty?
     get root_path
     assert flash.empty?                                        
-
   end  
+ 
 end
