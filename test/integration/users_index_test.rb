95095baae540d5ad57c437e6e71f7d82a7892f0a
James McCann <jamesredux@gmail.com>
2019-10-04 12:25:57 +0700

    destroy user test

--- test/integration/users_index_test.rb
+++ test/integration/users_index_test.rb
@@ -3,19 +3,32 @@ require 'test_helper'
 class UsersIndexTest < ActionDispatch::IntegrationTest
   
   def setup
-  	@user = users(:james)  	
+  	@admin = users(:james)
+    @non_admin  = users(:dale)  	
   end
 
   test "index including pagination" do 
-  	log_in_as(@user)
+  	log_in_as(@admin)
   	get users_path
   	assert_template 'users/index'
   	assert_select 'nav.pagination'
-
-  	User.page(1).per(15).each do |user|
+    first_page_of_users = User.page(1).per(15)
+    first_page_of_users.each do |user|
     	assert_select 'a[href=?]', user_path(user), text: user.name
- 		end
-
-
+      #this is not working now, it does not seem that admin is on first page??
+      unless user == @admin
+       assert_select 'a[href=?]', user_path(user), text: 'delete'
+      end  
+   	end
+    assert_difference 'User.count', -1 do 
+      delete user_path(@non_admin)
+    end  
   end	
+
+  test "index as non-admin" do
+    log_in_as(@non_admin)
+    get users_path
+    assert_select 'a', text: 'delete', count: 0
+  end
+
 end
